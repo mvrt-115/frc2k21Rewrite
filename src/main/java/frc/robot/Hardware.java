adcf4bc653f8e2beca60277db674ba3e9abf6b0a
Joshua Au <jau530@student.fuhsd.org>
2021-03-31 12:20:15 -0700

    Added to Hardware and created instance of Hopper  in RobotContainer

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -9,10 +9,18 @@ import com.ctre.phoenix.motorcontrol.can.BaseTalon;
 
 /** Add your docs here. */
 public class Hardware {
-    public static class ExampleSubsystem {
-        public static BaseTalon rightLeader;
+    // public static class ExampleSubsystem {
+    //     public static BaseTalon rightLeader;
 
-        // simulation
-        public static TalonSRXSimCollection rightMotorControllerSim;
+    //     // simulation
+    //     public static TalonSRXSimCollection rightMotorControllerSim;
+    // }
+
+    public static class Hopper {
+        public static BaseTalon right;
+        public static BaseTalon left;
+        
+        public static TalonSRXSimCollection rightSim;
+        public static TalonSRXSimCollection leftSim;
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.Hardware.Hopper;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,10 +17,12 @@ import frc.robot.subsystems.ExampleSubsystem;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  // // The robot's subsystems and commands are defined here...
+  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem, true);
+  // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem, true);
+
+  private final Hopper hopper = new Hopper();
 
   private Joystick joystick;
 
@@ -45,10 +44,10 @@ public class RobotContainer {
     // new JoystickButton(joystick, 1).whenPressed(new ExampleCommand(m_exampleSubsystem, true));
 
     // if you want to run a command when the button is released
-    new JoystickButton(joystick, 1).whenPressed(new ExampleCommand(m_exampleSubsystem, true)).whenReleased(new ExampleCommand(m_exampleSubsystem, false));
+  //   new JoystickButton(joystick, 1).whenPressed(new ExampleCommand(m_exampleSubsystem, true)).whenReleased(new ExampleCommand(m_exampleSubsystem, false));
 
-    SmartDashboard.putData("ExampleCommand", new ExampleCommand(m_exampleSubsystem, true));
-  }
+  //   SmartDashboard.putData("ExampleCommand", new ExampleCommand(m_exampleSubsystem, true));
+   }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -57,6 +56,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
