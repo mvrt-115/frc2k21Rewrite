c1eaf3a957d59be7653e9c82a451fb033b4bfae3
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2021-04-02 14:06:27 -0700

    Created Intake Hardware stuff

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -4,12 +4,28 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
+import com.ctre.phoenix.motorcontrol.can.BaseTalon;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /** Add your docs here. */
 public class Hardware {
-    // example
     public static class ExampleSubsystem {
-        public static WPI_TalonSRX rightLeader;
+        public static BaseTalon rightLeader;
+
+        // simulation
+        public static TalonSRXSimCollection rightMotorControllerSim;
+    }
+    public static class Intake {
+        public static BaseTalon pivot;
+        public static BaseTalon roller;
+        public static BaseTalon funnel;
+
+        public static DigitalInput limitSwitchTop;
+        public static DigitalInput limitSwitchBottom;
+
+        public static TalonSRXSimCollection pivotSim;
+
     }
 }
