d36cf967c599cccb2a050305b0cc458a5c73f1d9
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2021-04-06 12:20:54 -0700

    started implementing subsystem methods

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,10 +4,13 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Hardware;
@@ -15,18 +18,23 @@ import frc.robot.Robot;
 
 public class Intake extends SubsystemBase {
   public static enum IntakeState {
-    DISABLED, DEPLOYING, DEPLOYED, STOWING, STOWED, INTAKING
+    DISABLED, DEPLOYING, STOWING, INTAKING
   }
 
+  /**The state of the intake */
   private IntakeState state;
+
+  private boolean limitSwitchInitial;
+  private double feedForward;
 
   /** Creates a new Intake. */
 
   public Intake() {
+    // initialize differently based on if it is a real or simulated robot
     if(Robot.isReal()) {
-      Hardware.Intake.pivot = new TalonFX(Constants.Intake.PIVOT_ID);
-      Hardware.Intake.roller = new TalonFX(Constants.Intake.ROLLER_ID);
-      Hardware.Intake.funnel = new TalonFX(Constants.Intake.FUNNEL_ID);
+      Hardware.Intake.pivot = new TalonSRX(Constants.Intake.PIVOT_ID);
+      Hardware.Intake.roller = new TalonSRX(Constants.Intake.ROLLER_ID);
+      Hardware.Intake.funnel = new TalonSRX(Constants.Intake.FUNNEL_ID);
 
       Hardware.Intake.pivot.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
     } else {
@@ -37,13 +45,18 @@ public class Intake extends SubsystemBase {
       Hardware.Intake.pivotSim = ((WPI_TalonSRX)Hardware.Intake.pivot).getSimCollection();
     }
 
+    // reset to factory defaults
     Hardware.Intake.pivot.configFactoryDefault();
     Hardware.Intake.roller.configFactoryDefault();
     Hardware.Intake.funnel.configFactoryDefault();
 
-    Hardware.Intake.pivot.config_kP(0, Constants.Intake.kP);
-    Hardware.Intake.pivot.config_kI(0, Constants.Intake.kI);
-    Hardware.Intake.pivot.config_kD(0, Constants.Intake.kD);
+    // configure PID constants and feed forward to compensate for gravity
+    Hardware.Intake.pivot.config_kP(0, Constants.Intake.P);
+    Hardware.Intake.pivot.config_kI(0, Constants.Intake.I);
+    Hardware.Intake.pivot.config_kD(0, Constants.Intake.D);
+    Hardware.Intake.pivot.config_kF(0, Constants.Intake.F);
+
+    limitSwitchInitial = Hardware.Intake.limitSwitchBottom.get();
 
     setState(IntakeState.DISABLED);
   }
@@ -51,38 +64,95 @@ public class Intake extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    switch( state )
+    {
+      case INTAKING:
+        intake();
+        break;
+
+      case STOWING:
+        stow();
+        break;
+
+      case DEPLOYING:
+        deploy();
+        break;
+
+      case DISABLED:
+        stopIntake();
+        stopPivot();
+        break;
+
+      default: 
+        break;
+    }
+    log();
   }
 
-  public void deploying()
+  /**
+   * Deploys the intake, if the intake has reached the bottom the intake is deployed and will begin to intake
+   */
+  public void deploy() {
+    if(Hardware.Intake.limitSwitchBottom.get() != limitSwitchInitial 
+        || getPivotTicks() >= degreesToTicks(Constants.Intake.PIVOT_DEPLOYED_DEGREES)) {
+      stopPivot();
+      setState(IntakeState.INTAKING);
+      intake();
+    } else {
+      pivot(Constants.Intake.PIVOT_DEPLOYED_DEGREES);
+    }    
+  }
+
+  /**
+   * Stows the intake until the intake has reached the top
+   */
+  public void stow() {
+    if(getPivotTicks() <= Constants.Intake.PIVOT_STOWED_DEGREES) {
+      stopPivot();
+      setState(IntakeState.DISABLED);
+    } else {
+      pivot(Constants.Intake.PIVOT_STOWED_DEGREES);
+    }    
+  }
+
+  /**
+   * Pivots the intake to a desired target angle
+   * @param target_degrees  The target angle in degrees
+   */
+  public void pivot(double target_degrees)
   {
-    //Move pivot down until it reaches the bottom.
-    //Once pivot has reached he bottom, set the state to INTAKING
+    Hardware.Intake.pivot.set(
+      ControlMode.Position, 
+      degreesToTicks(target_degrees), 
+      DemandType.ArbitraryFeedForward, 
+      feedForward
+    );
   }
 
+  /**
+   * Runs the intake by running it at a constant speed
+   */
   public void intake()
   {
-    //run intake at a speed
-  }
-
-  public void stowing()
-  {
-    //move pivot up until it has reached the top
-    //Once pivot has reached the top, set the state to STOWED
+    Hardware.Intake.funnel.set(ControlMode.PercentOutput, Constants.Intake.INTAKE_SPEED);
+    Hardware.Intake.roller.set(ControlMode.PercentOutput, Constants.Intake.INTAKE_SPEED);
   }
   
+  /**
+   * Stops the pivot motor
+   */
   public void stopPivot()
   {
-    //set speed of pivot to 0
+    Hardware.Intake.pivot.set(ControlMode.PercentOutput, 0);
   }
 
-  public void stopFunnel()
+  /**
+   * Stops the intake motors
+   */
+  public void stopIntake()
   {
-    //set speed of funnel to 0
-  }
-
-  public void stopRoller()
-  {
-    //set roller speed to 0
+    Hardware.Intake.funnel.set(ControlMode.PercentOutput, 0);
+    Hardware.Intake.roller.set(ControlMode.PercentOutput, 0);
   }
 
   /**
@@ -99,4 +169,39 @@ public class Intake extends SubsystemBase {
     this.state = state;
   }
 
+  /**
+   * Converts degrees to ticks
+   * @param degrees an anlge in degrees
+   * @return        the amount of ticks in that angle
+   */
+  public double degreesToTicks(double degrees) {
+    return degrees / 360 * Constants.Intake.PIVOT_TICKS_PER_REVOLUTION * Constants.Intake.PIVOT_GEAR_RATIO;
+  }
+
+  /**
+   * Finds the current position of the pivot in ticks
+   * @return the current position of the pivot in ticks
+   */
+  public double getPivotTicks() {
+    return Hardware.Intake.pivot.getSelectedSensorPosition();
+  }
+
+  /**
+   * Converts ticks to degrees
+   * @param ticks a position in ticks
+   * @return      the angle in degrees of the position
+   */
+  public double ticksToDegrees(double ticks) {
+    return 360 * ticks / (Constants.Intake.PIVOT_TICKS_PER_REVOLUTION * Constants.Intake.PIVOT_GEAR_RATIO);
+  }
+
+  /**
+   * Logs data about the subsystem to smart dashboard
+   */
+  public void log() {
+    SmartDashboard.putString("Intake State", state.toString());
+    SmartDashboard.putNumber("Intake Position (degrees)", ticksToDegrees(getPivotTicks()));
+    SmartDashboard.putBoolean("Intake Running", getState() == IntakeState.INTAKING);
+    SmartDashboard.putNumber("Pivot Output", Hardware.Intake.pivot.getMotorOutputPercent());
+  }
 }
