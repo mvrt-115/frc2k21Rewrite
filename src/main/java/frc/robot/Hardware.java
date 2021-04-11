027e397474b0eff8a61fad3513e9c8f33b7b8c5b
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2021-04-10 18:25:28 -0700

    moved hardware variables to intake subsystem

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -6,9 +6,6 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
 import com.ctre.phoenix.motorcontrol.can.BaseTalon;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-
 /** Add your docs here. */
 public class Hardware {
     public static class ExampleSubsystem {
@@ -16,20 +13,5 @@ public class Hardware {
 
         /** simulation */
         public static TalonSRXSimCollection rightMotorControllerSim;
-    }
-    public static class Intake {
-        /** Pivot Talon */
-        public static BaseTalon pivot;
-
-        /** Roller Talon */
-        public static BaseTalon roller;
-
-        /** Funnel Talon */
-        public static BaseTalon funnel;
-
-        public static DigitalInput limitSwitchBottom;
-
-        public static TalonSRXSimCollection pivotSim;
-
     }
 }
